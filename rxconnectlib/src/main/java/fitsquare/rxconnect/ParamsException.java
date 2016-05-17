b3e06dd3b89d8aae777232921b37a0ec0b7c4fd5
Mohit Badwal <badwalmohit@gmail.com>
2016-05-17 10:25:16 +0530

    Added Project

---
+++ rxconnectlib/src/main/java/fitsquare/rxconnect/ParamsException.java
@@ -0,0 +1,15 @@
+package fitsquare.rxconnect;
+
+/**
+ * Created by MohitBadwal on 16-May-16.
+ */
+public class ParamsException extends Exception {
+    public ParamsException()
+    {
+
+    }
+    public String toString()
+    {
+        return "Exception in getParam(String,String)";
+    }
+}
